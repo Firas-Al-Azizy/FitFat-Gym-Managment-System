6547b6d99f0b16c70419dfb66a38100608cafbe0
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-18 14:01:33 +0300

    adding connection class

---
+++ FFGMS/db_con/cls_db.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace FFGMS.db_con
+{
+    class cls_db
+    {
+        static SqlConnection con;
+
+        SqlCommand Scom;
+
+        public static void setConnection()
+        {
+            SqlConnection con = new SqlConnection(@"server=.;
+        database=ffgms ; integrated security=true");
+            // SqlConnection con = new SqlConnection(@"server=ALI-PC\SQLEXPRESS;
+            //database=ali_db ; integrated security=true");
+        }
+
+        public static object exe_pro(SqlCommand com, int type)
+        {
+            setConnection();
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+                com.Connection = con;
+            }
+            switch (type)
+            {
+                case 1:
+                    {
+
+
+                        int r = com.ExecuteNonQuery();
+                        con.Close();
+                        return r;
+
+
+                    }
+                case 2:
+                    {
+                        DataTable dt = new DataTable();
+
+                        SqlDataReader sDR = com.ExecuteReader();
+                        dt.Load(sDR);
+
+                        con.Close();
+                        return dt;
+
+                    }
+
+            }
+            return -1;
+        }
+    }
+}

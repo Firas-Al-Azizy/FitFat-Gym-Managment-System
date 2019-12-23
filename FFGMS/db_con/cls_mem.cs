a71e0668a68d05642fadb7873d986806a3b3c9b0
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-23 08:24:03 +0300

    adding class for membership

---
+++ FFGMS/db_con/cls_mem.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FFGMS.db_con
+{
+    class cls_mem
+    {
+        public string mac_name { get; set; }
+        public byte[] mac_img { get; set; }
+        public static int InsertData(cls_mem o)
+        {
+            var EffectedRows = 0;
+
+            SqlParameter p;
+            try
+            {
+                using (SqlCommand SqlCmd = new SqlCommand())
+                {
+                    {
+                        var withBlock = SqlCmd;
+                        withBlock.CommandText = "[Pkgmem.insert]";
+                        withBlock.CommandType = CommandType.StoredProcedure;
+                    }
+                    p = new SqlParameter("@Pmac_name", SqlDbType.NVarChar)
+                    {
+                        Value = o.mac_name
+                    };
+                    SqlCmd.Parameters.Add(p);
+                    p = new SqlParameter("@Pmac_img", SqlDbType.Image)
+                    {
+                        Value = o.mac_img
+                    };
+                    SqlCmd.Parameters.Add(p);
+
+
+                    EffectedRows = (int)cls_db.exe_pro(SqlCmd, 1);
+                }
+            }
+
+            catch (Exception)
+            {
+                return EffectedRows;
+
+
+            }
+            return EffectedRows;
+
+        }
+        public static int updateData(cls_mem o)
+        {
+            var EffectedRows = 0;
+
+            SqlParameter p;
+            try
+            {
+                using (SqlCommand SqlCmd = new SqlCommand())
+                {
+                    {
+                        var withBlock = SqlCmd;
+                        withBlock.CommandText = "[Pkgmem.update]";
+                        withBlock.CommandType = CommandType.StoredProcedure;
+                    }
+                    p = new SqlParameter("@Pmac_name", SqlDbType.NVarChar)
+                    {
+                        Value = o.mac_name
+                    };
+                    SqlCmd.Parameters.Add(p);
+                    p = new SqlParameter("@Pmac_img", SqlDbType.Image)
+                    {
+                        Value = o.mac_img
+                    };
+                    SqlCmd.Parameters.Add(p);
+
+
+                    EffectedRows = (int)cls_db.exe_pro(SqlCmd, 1);
+                }
+            }
+
+            catch (Exception)
+            {
+                return EffectedRows;
+
+
+            }
+            return EffectedRows;
+
+        }
+
+        public static DataTable selectAll()
+        {
+            DataTable dt = new DataTable();
+
+
+            try
+            {
+                using (SqlCommand SqlCmd = new SqlCommand())
+                {
+                    {
+                        var withBlock = SqlCmd;
+                        withBlock.CommandText = "[Pkgmem.SelectAll]";
+                        withBlock.CommandType = CommandType.StoredProcedure;
+                    }
+
+                    dt = (DataTable)cls_db.exe_pro(SqlCmd, 2);
+                }
+            }
+
+            catch (Exception)
+            {
+                dt = new DataTable();
+
+
+            }
+            return dt;
+
+        }
+    }
+}
